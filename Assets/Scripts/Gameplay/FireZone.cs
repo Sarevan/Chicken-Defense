383f5d0ff899015f9d56fc94a0d07a96f41ecb89
Sarevan <94778015+Sarevan@users.noreply.github.com>
2022-10-13 18:00:58 +0300

    Added FireZone on the scene

---
+++ Assets/Scripts/Gameplay/FireZone.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class FireZone : MonoBehaviour
+    {
+        [SerializeField] private int steps = 1;
+        [SerializeField] private float radius = 1;
+       
+
+
+        [SerializeField] private LineRenderer circleRenderer;
+
+        public void Start()
+        {
+            DrawCircle(steps,radius);
+        }
+
+        public void DrawCircle(int steps, float radius)
+        {
+            circleRenderer.positionCount = steps;
+            for (int currentStep = 0; currentStep < steps; currentStep++)
+            {
+                float circumferenceProgress = (float) currentStep / steps;
+
+                float currentRadian = circumferenceProgress * 2 * Mathf.PI;
+                float xScaled = Mathf.Cos(currentRadian);
+                float zScaled = Mathf.Sin(currentRadian);
+
+                float x =  xScaled * radius;
+                float z = zScaled * radius;
+
+                Vector3 currentPosition = new Vector3(x, 0, z);
+                circleRenderer.SetPosition(currentStep,currentPosition);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Hero/CustomInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gameplay.Hero
+{
+    [CustomEditor(typeof(FireZone))]
+    public class CustomInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            FireZone exam = (FireZone) target;
+            if (GUILayout.Button("Start"))
+            {
+                exam.Start();
+            }
+        }
+    }
+}
